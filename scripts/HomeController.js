561a589ac9e214eb605bb3261d51747d5cc16c13
Tyler.Austgen <taustgen@gmail.com>
2017-03-09 10:10:31 -0800

    Beautified html and js files

--- scripts/HomeController.js
+++ scripts/HomeController.js
@@ -4,21 +4,21 @@ angular
 
 function HomeController() {
     var vm = this;
-    vm.order="3"
-    vm.hide= true;
+    vm.order = "3"
+    vm.hide = true;
     vm.toDoList = [];
     vm.priorities = [
         ["Urgent", 1, "alert alert-danger"],
         ["High", 2, "alert alert-warning"],
         ["Low", 3, "alert alert-success"]
     ];
-    vm.count= 0
+    vm.count = 0
     vm.add = function append() {
         if (vm.task) {
-            vm.toDoList.push([vm.task, vm.priority[1], vm.priority[2],vm.count]);
+            vm.toDoList.push([vm.task, vm.priority[1], vm.priority[2], vm.count]);
             vm.task = "";
             vm.priority = "";
-            vm.hide=false;
+            vm.hide = false;
             vm.count++;
         }
     }
@@ -31,10 +31,10 @@ function HomeController() {
     vm.az = function az() {
         vm.order = "0";
     }
-    vm.oldest= function Oldest(){
-        vm.order="3";
+    vm.oldest = function Oldest() {
+        vm.order = "3";
     }
-    vm.newest= function Newest(){
-      vm.order="-3"
+    vm.newest = function Newest() {
+        vm.order = "-3"
     }
 }

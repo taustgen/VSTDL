6be76934aab4c25c212cdb8ce6647dbd6626d13a
Tyler.Austgen <taustgen@gmail.com>
2017-03-29 15:32:49 -0700

    Formatted to John Papa style guide

---
+++ app/todo.controller.js
@@ -0,0 +1,47 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('todo')
+        .controller('TodoController', TodoController);
+
+    TodoController.$inject = [];
+
+    /* @ngInject */
+    function TodoController() {
+        var vm = this;
+        vm.order = "3"
+        vm.hide = true;
+        vm.toDoList = [];
+        vm.priorities = [
+            ["Urgent", 1, "alert alert-danger"],
+            ["High", 2, "alert alert-warning"],
+            ["Low", 3, "alert alert-success"]
+        ];
+        vm.count = 0;
+        vm.add = function append() {
+            if (vm.task) {
+                vm.toDoList.push([vm.task, vm.priority[1], vm.priority[2], vm.count]);
+                vm.task = "";
+                vm.priority = "";
+                vm.hide = false;
+                vm.count++;
+            }
+        }
+        vm.high = function high() {
+            vm.order = "1";
+        }
+        vm.low = function low() {
+            vm.order = "-1";
+        }
+        vm.az = function az() {
+            vm.order = "0";
+        }
+        vm.oldest = function Oldest() {
+            vm.order = "3";
+        }
+        vm.newest = function Newest() {
+            vm.order = "-3"
+        }
+    }
+})();
